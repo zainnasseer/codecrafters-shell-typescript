4189f82aa96a1919d5cb2d9c09a04050353a1d12
zainnasseer <zainnaser18@gmail.com>
2026-09-19 21:54:07 +0300

    Save local changes before pushing

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,12 @@
 import { createInterface } from "readline";
 
+// in js it would be:
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   prompt: '$ '
+// });
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -9,11 +16,17 @@ const rl = createInterface({
 // TODO: Uncomment the code below to pass the first stage
 rl.prompt(); // shows the prompt for the first time.
 
+// Once an interface is created, the 'line' event can be used to read input line-by-line.
+// The 'line' event fires each time the user presses Enter
 rl.on("line", (command) => {
-  // prints the command followed by ": command not found" to the console
-  console.log(`${command}: command not found`);
-
-  rl.prompt(); //The readline interface automatically waits
+  if (command.trim() === "exit") {
+    rl.close();
+    return;
+  } else {
+    // prints the command followed by ": command not found" to the console
+    console.log(`${command}: command not found`);
+    rl.prompt();
+  } //The readline interface automatically waits
   //  for the next input after each rl.prompt() call,
   //  keeping the shell running indefinitely.
   // so its core function is to display a prompt (e.g "$ " or any string)

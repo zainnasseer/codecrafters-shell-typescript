b9acacc1f2fd057f0a3b6a359c6d5c4e9148b3cd
zainnasseer <zainnaser18@gmail.com>
2026-09-19 19:25:41 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,8 +7,15 @@ const rl = createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.prompt();
+rl.prompt(); // shows the prompt for the first time.
 
 rl.on("line", (command) => {
+  // prints the command followed by ": command not found" to the console
   console.log(`${command}: command not found`);
+
+  rl.prompt(); //The readline interface automatically waits
+  //  for the next input after each rl.prompt() call,
+  //  keeping the shell running indefinitely.
+  // so its core function is to display a prompt (e.g "$ " or any string)
+  // and wait for input.
 });
